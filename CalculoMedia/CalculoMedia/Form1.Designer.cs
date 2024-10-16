956a4282112546be553da3c0cf1b150ff892ba6d
John Allan Travinski <john@mksoftwares.com.br>
2024-10-16 10:55:50 -0300

    Add files via upload

---
+++ CalculoMedia/CalculoMedia/Form1.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace CalculoMedia
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            label6 = new Label();
+            label7 = new Label();
+            timer = new System.Windows.Forms.Timer(components);
+            guna2BorderlessForm1 = new Guna.UI2.WinForms.Guna2BorderlessForm(components);
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = SystemColors.ActiveCaptionText;
+            label1.Font = new Font("Segoe UI", 17F);
+            label1.ForeColor = SystemColors.ControlLightLight;
+            label1.Location = new Point(90, 35);
+            label1.Name = "label1";
+            label1.Size = new Size(192, 31);
+            label1.TabIndex = 0;
+            label1.Text = "Calculo de Média";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = SystemColors.ActiveCaptionText;
+            label2.ForeColor = SystemColors.ControlLightLight;
+            label2.Location = new Point(21, 108);
+            label2.Name = "label2";
+            label2.Size = new Size(46, 15);
+            label2.TabIndex = 1;
+            label2.Text = "NOTA 1";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = SystemColors.ActiveCaptionText;
+            label3.ForeColor = SystemColors.ControlLightLight;
+            label3.Location = new Point(21, 203);
+            label3.Name = "label3";
+            label3.Size = new Size(46, 15);
+            label3.TabIndex = 3;
+            label3.Text = "NOTA 3";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.BackColor = SystemColors.ActiveCaptionText;
+            label4.ForeColor = SystemColors.ControlLightLight;
+            label4.Location = new Point(21, 156);
+            label4.Name = "label4";
+            label4.Size = new Size(46, 15);
+            label4.TabIndex = 3;
+            label4.Text = "NOTA 2";
+            label4.Click += label4_Click;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.BackColor = SystemColors.ActiveCaptionText;
+            label5.ForeColor = SystemColors.ControlLightLight;
+            label5.Location = new Point(21, 252);
+            label5.Name = "label5";
+            label5.Size = new Size(46, 15);
+            label5.TabIndex = 4;
+            label5.Text = "NOTA 4";
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = SystemColors.InactiveCaptionText;
+            textBox1.ForeColor = SystemColors.Window;
+            textBox1.Location = new Point(90, 105);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            textBox2.BackColor = SystemColors.MenuText;
+            textBox2.ForeColor = SystemColors.Window;
+            textBox2.Location = new Point(90, 153);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(100, 23);
+            textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            textBox3.BackColor = SystemColors.MenuText;
+            textBox3.ForeColor = SystemColors.Window;
+            textBox3.Location = new Point(90, 203);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(100, 23);
+            textBox3.TabIndex = 7;
+            // 
+            // textBox4
+            // 
+            textBox4.BackColor = SystemColors.InfoText;
+            textBox4.ForeColor = SystemColors.Window;
+            textBox4.Location = new Point(90, 244);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(100, 23);
+            textBox4.TabIndex = 8;
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.ActiveCaptionText;
+            button1.Font = new Font("Segoe UI", 13F);
+            button1.ForeColor = SystemColors.ControlLightLight;
+            button1.Location = new Point(31, 359);
+            button1.Name = "button1";
+            button1.Size = new Size(117, 34);
+            button1.TabIndex = 9;
+            button1.Text = "CALCULAR";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = SystemColors.ActiveCaptionText;
+            button2.Font = new Font("Segoe UI", 13F);
+            button2.ForeColor = SystemColors.ControlLightLight;
+            button2.Location = new Point(228, 359);
+            button2.Name = "button2";
+            button2.Size = new Size(102, 34);
+            button2.TabIndex = 10;
+            button2.Text = "LIMPAR";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.BackColor = SystemColors.ActiveCaptionText;
+            label6.Font = new Font("Segoe UI", 13F);
+            label6.ForeColor = SystemColors.ControlLightLight;
+            label6.Location = new Point(258, 119);
+            label6.Name = "label6";
+            label6.Size = new Size(81, 25);
+            label6.TabIndex = 11;
+            label6.Text = "MÉDIA : ";
+            label6.Click += label6_Click;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.BackColor = SystemColors.ActiveCaptionText;
+            label7.Font = new Font("Segoe UI", 15F);
+            label7.ForeColor = SystemColors.ControlLightLight;
+            label7.Location = new Point(278, 156);
+            label7.Name = "label7";
+            label7.Size = new Size(34, 28);
+            label7.TabIndex = 12;
+            label7.Text = ". . .";
+            label7.Click += label7_Click;
+            // 
+            // timer
+            // 
+            timer.Tick += timer_Tick;
+            // 
+            // guna2BorderlessForm1
+            // 
+            guna2BorderlessForm1.AnimationInterval = 250;
+            guna2BorderlessForm1.AnimationType = Guna.UI2.WinForms.Guna2BorderlessForm.AnimateWindowType.AW_CENTER;
+            guna2BorderlessForm1.BorderRadius = 20;
+            guna2BorderlessForm1.ContainerControl = this;
+            guna2BorderlessForm1.DockIndicatorTransparencyValue = 0.6D;
+            guna2BorderlessForm1.ResizeForm = false;
+            guna2BorderlessForm1.TransparentWhileDrag = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(384, 450);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private Button button1;
+        private Button button2;
+        private Label label6;
+        private Label label7;
+        private System.Windows.Forms.Timer timer;
+        private Guna.UI2.WinForms.Guna2BorderlessForm guna2BorderlessForm1;
+    }
+}
---
+++ CalculoMedia/CalculoMedia/Particle.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+public class Particle
+{
+    public Point Position { get; set; }
+    public Point Velocity { get; set; }
+    public Color Color { get; set; }
+    public int Size { get; set; }
+
+    public Particle(int x, int y)
+    {
+        Position = new Point(x, y);
+        Velocity = new Point(new Random().Next(-2, 3), new Random().Next(-2, 3));
+        Color = Color.FromArgb(100, 255, 255, 255); // Cor semi-transparente
+        Size = new Random().Next(5, 15);
+    }
+
+    public Point GetPosition()
+    {
+        return Position;
+    }
+
+    public Point GetPosition2()
+    {
+        return Position;
+    }
+
+    public Point GetVelocity()
+    {
+        return Velocity;
+    }
+
+    public Point GetVelocity2()
+    {
+        return Velocity;
+    }
+
+    public void Update(Point position, Point position2, Point velocity, Point velocity2)
+    {
+        position.X += Velocity.X;
+        position2.Y += Velocity.Y;
+
+        // Repare que as partículas "rebaterão" nas bordas do formulário
+        if (Position.X < 0 || Position.X > 800) velocity.X = -Velocity.X;
+        if (Position.Y < 0 || Position.Y > 600) velocity2.Y = -Velocity.Y;
+    }
+}
